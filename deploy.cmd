bc80fc63142757849a41ba0dfbc2877050fc0207
David Fowler <davidfowl@gmail.com>
2012-06-20 08:48:53 -0700

    Removed msbuild's full path

--- deploy.cmd
+++ deploy.cmd
@@ -1,2 +1,2 @@
 @echo off
-"%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" SimpleWebApplication\SimpleWebApplication.csproj /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="%TARGET%";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Debug;SolutionDir="%SOURCE%"
+MSBuild SimpleWebApplication\SimpleWebApplication.csproj /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="%TARGET%";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Debug;SolutionDir="%SOURCE%"
