81decbd47157139569585dcf9e981f442964debe
David Fowler <davidfowl@gmail.com>
2012-06-19 21:53:59 -0700

    Added deployment batch file

---
+++ deploy.cmd
@@ -0,0 +1,1 @@
+"%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" SimpleWebApplication\SimpleWebApplication.csproj /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="%TARGET%";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release;SolutionDir="%SOURCE%"
