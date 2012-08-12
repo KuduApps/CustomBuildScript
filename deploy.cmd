2c156bd097f25eb175e746cfa1de4c36bbba3d0c
David Fowler <davidfowl@gmail.com>
2012-08-12 14:20:02 -0700

    Bail out if the target or source isn't defined.

--- deploy.cmd
+++ deploy.cmd
@@ -1,2 +1,4 @@
 @echo off
+IF "%TARGET%" == "" exit 1
+IF "%SOURCE%" == "" exit 1
 MSBuild SimpleWebApplication\SimpleWebApplication.csproj /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="%TARGET%";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Debug;SolutionDir="%SOURCE%"
